4860ebcc2cbe93393ac7117a040a1468e2ddde38
unknown <faisaljawed2000@gmail.com>
2024-10-17 12:13:32 +0500

    Routing issue

--- src/Router.tsx
+++ src/Router.tsx
@@ -15,11 +15,11 @@ const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<RootLayout />}>
       <Route index={true} element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/contact" element={<Contacts />} />
+      <Route path="about" element={<About />} />
+      <Route path="contact" element={<Contacts />} />
       {/* <Route path="/education" element={<Educations />} /> */}
-      <Route path="/skills" element={<Skills />} />
-      <Route path="/project" element={<Projects />} />
+      <Route path="skills" element={<Skills />} />
+      <Route path="project" element={<Projects />} />
     </Route>
   )
 );
